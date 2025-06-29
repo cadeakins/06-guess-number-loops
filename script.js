535dc9a1761a6234f1a4c929597e0945af95b2f6
Cade Medearis <108916110+cadeakins@users.noreply.github.com>
2025-06-29 22:12:57 +0000

    Completed assignment

--- script.js
+++ script.js
@@ -2,4 +2,24 @@ document.getElementById('start-button').addEventListener('click', function() {
   // Generate a random number between 1 and 10
   const secretNumber = Math.floor(Math.random() * 10) + 1;
 
+  let attempts = 0;
+  let guess = 0;
+
+  for (let i = 0; i <= 5; i++) {
+    guess = parseInt(prompt("Guess a number between 1 and 10:"), 10);
+    attempts++;
+
+
+    if (guess === secretNumber) {
+      document.getElementById('message').textContent = `Congratulations! You guessed the number ${secretNumber} in ${attempts} attempt${attempts > 1 ? 's' : ''}.`;
+      return; // Exit the loop if the guess is correct
+    } else if (guess > secretNumber) {
+      alert("Too high! Try again.");
+    } else if (guess < secretNumber) {
+      alert("Too low! Try again.");
+    }
+  }
+
+
+  document.getElementById('message').textContent = `Sorry, you've used all your attempts. The secret number was ${secretNumber}.`;
 });
